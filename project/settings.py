561b3b0d81bb00b3154d9ffef49b782f14ed446d
Anup Dahal <adahal39@gmail.com>
2023-10-10 11:24:06 +0000

    Fixed some changes in server

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',  'django-insecure-yurr6h861v9#g-e#qr6v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
