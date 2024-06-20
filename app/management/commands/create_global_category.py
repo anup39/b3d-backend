3f63570cef4cff8b70fd4cf540b0a342fe6762a5
anup39 <adahal39@gmail.com>
2024-06-20 17:36:19 +0200

    fixed the managemnt command

--- app/management/commands/create_global_category.py
+++ app/management/commands/create_global_category.py
@@ -1,9 +1,38 @@
 # app/management/commands/create_global_category.py
 
 from django.core.management.base import BaseCommand
+from django.db.models import Subquery, OuterRef
+from app.models import PolygonData ,LineStringData, PointData ,Category
 
 class Command(BaseCommand):
-    help = 'Displays a simple greeting'
+    help = 'Update global_category in PolygonData model from related Category model'
 
-    def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS('Hello, World! I am running the command'))
+    def handle(self, *args, **options):
+        # Subquery to get the global_category of the related Category
+        subquery = Category.objects.filter(id=OuterRef('category_id')).values('global_category')[:1]
+
+        # Update all PolygonData objects in a single query
+        PolygonData.objects.update(global_category=Subquery(subquery))
+
+
+        self.stdout.write(self.style.SUCCESS('Successfully updated global_category for all PolygonData objects'))
+
+        PointData.objects.update(global_category=Subquery(subquery))
+
+        self.stdout.write(self.style.SUCCESS('Successfully updated global_category for all PointData objects'))
+
+        LineStringData.objects.update(global_category=Subquery(subquery))
+
+        self.stdout.write(self.style.SUCCESS('Successfully updated global_category for all LineData objects'))
+
+        # for point in PointData.objects.all():
+        #     if point.category and point.category.global_category:
+        #         point.global_category = point.category.global_category
+        #         point.save()
+        # self.stdout.write(self.style.SUCCESS('Successfully updated global_category for all PointData objects'))
+
+        # for line in LineStringData.objects.all():
+        #     if line.category and line.category.global_category:
+        #         line.global_category = line.category.global_category
+        #         line.save()
+        # self.stdout.write(self.style.SUCCESS('Successfully updated global_category for all LineData objects'))
